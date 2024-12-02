4ce01cb99faec300b6a1f7a087622210be39dbbf
bawiiman <rockyman0504@naver.com>
2024-12-02 13:43:02 +0900

    [FEAT] 캐릭터 접속

--- Node/playerServer.js
+++ Node/playerServer.js
@@ -1,9 +1,10 @@
-const WebSockect = require('ws');
+const WebSocket = require('ws');
+const iconv = require('iconv-lite');
 
 class GameServer {
 
     constructor(port){
-        this.wss = new WebSockect.Server({port});
+        this.wss = new WebSocket.Server({ port });
         this.clients = new Set();       //1차 단순 클라이언트 관리
         this.players = new Map();       //2차 플레이어 위치 관리 추가
         this.setupServerEvent();
@@ -22,7 +23,7 @@ class GameServer {
             this.players.set(playerId,
                 {
                     socket: socket,
-                    position: { x: 0, y: 0, z: 0}
+                    position: { x: 0, y: 0, z: 0 }
                 }
             );
 
@@ -36,29 +37,59 @@ class GameServer {
             };
             socket.send(JSON.stringify(welcomeData));
 
-            socket.on(`message`, (message) => {
-                try{
-                    const data = Json.parse(message);
+            //새로 추가 : 현재 접속해 있는 다른 플레이어들의 위치 정보 전송
+            this.players.forEach((playerInfo, id) => {
+                if(id !== playerId) {
+                    socket.send(JSON.stringify({
+                        type: 'playerPosition',
+                        playerId: id,
+                        position: playerInfo.position
+                    }));
+                }
+            });
 
-                    const messageString = iconv.decode(Buffer.from(data.message), 'euc-kr');
-                    console.log(`수신된 메세지 : `, messageString);
-                    this.broadcast({
-                        type: `chat`,
-                        message: messageString
-                    });
-                } catch(error){
-                    const messageString = iconv.decode(message, 'euc-kr');
-                    console.log(`수신된 메세지 :`, messageString);
-                    this.broadcast({
-                        type: `chat`,
-                        message: messageString
-                    });
+            socket.on(`message`, (message) => {
+                try {
+                    const data = JSON.parse(message);
+
+                    //switch 문으로 메세지 타입 처리 추가
+                    switch(data.type) {
+                        case 'chat':
+                            try {
+                                const messageString = iconv.decode(Buffer.from(data.message), 'euc-kr');
+                                console.log(`수신된 메세지 : `, messageString);
+                                this.broadcast({
+                                    type: `chat`,
+                                    message: messageString
+                                });
+                            } catch(error) {
+                                const messageString = iconv.decode(message, 'euc-kr');
+                                console.log(`수신된 메세지 :`, messageString);
+                                this.broadcast({
+                                    type: `chat`,
+                                    message: messageString
+                                });
+                            }
+                            break;
+
+                        case 'playerPosition':
+                            if(this.players.has(playerId)) {
+                                this.players.get(playerId).position = data.position;  //플레이어 위치 업데이트
+                                //다른 모든 클라이언트에게 위치 정보 전송
+                                this.broadcast({
+                                    type: 'playerPosition',
+                                    playerId: playerId,
+                                    position: data.position
+                                });
+                            }
+                    } 
+                } catch(error) {
+                    console.error('메세지 처리 중 오류');    
                 }
             });
 
             socket.on(`close`, () => {
-                this.clients.delete(socket);        //클라이언트 제거
-
+                this.clients.delete(socket);  //클라이언트 제거
                 this.players.delete(playerId);
                 this.broadcast({
                     type: 'playerDisconnect',
@@ -66,23 +97,20 @@ class GameServer {
                 });
                 console.log(`클라이언트 퇴장 ID : ${playerId}, 현재 접속자 : ${this.clients.size}`);
             });
-        })
+        });
     }
 
-    broadcast(data)         //브로드 캐스트 함수 설정
-    {
+    broadcast(data) {  //브로드 캐스트 함수 설정
         const message = JSON.stringify(data);
-        this.clients.forEach(clients => {
-            if(clients.readyState == WebSockect.OPEN)
-            {
-                clients.send(message);
+        this.clients.forEach(client => {
+            if(client.readyState == WebSocket.OPEN) {
+                client.send(message);
             }
         });
     }
 
-    generatePlayerId()
-    {
-        return `player_` + Math.random().toString(36),substr(2, 9);
+    generatePlayerId() {
+        return `player_` + Math.random().toString(36).substr(2, 9);
     }
 }
 
